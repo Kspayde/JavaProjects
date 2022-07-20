4b262b67165f11640f2f1c7ec376e8aae9007133
kamackey1988 <kimberlyspayde18@yahoo.com>
2022-07-20 15:42:47 -0400

    newfiles

---
+++ sectionone/Workbook1_1.java
@@ -0,0 +1,11 @@
+public class Workbook1_1 {
+
+    public static void main(String[] args) {
+        System.out.println("printing is fun");
+        System.out.println("Java > python ");
+        System.out.println("I spilled Java all over my paper ");
+        System.out.println("My dog ate my java");
+        
+    }
+
+}
---
+++ sectionone/YourInitials.java
@@ -0,0 +1,12 @@
+public class YourInitials {
+
+    public static void main(String[] args) {
+        System.out.println("K   K  M   M");
+        System.out.println("K  K   MM MM");
+        System.out.println("K K    M M M");
+        System.out.println("KK     M   M");
+        System.out.println("K K    M   M");
+        System.out.println("K  K   M   M");
+        System.out.println("K   K  M   M");
+    }
+}
